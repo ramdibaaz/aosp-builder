0cd9a86b7a2a9fd7a4c4c42c9a0ad65b9313b020
ramdibaaz <officialpkkmkb@gmail.com>
2021-05-18 13:07:09 +0000

    Fix For False notifications after kill

--- extra.sh
+++ extra.sh
@@ -12,23 +12,23 @@
         echo '++++++++NPROC++++++++'
         nproc
         free -h
-        
+
         echo '++++++++IPINFO++++++++'
         curl ipinfo.io/?token=24b85fe6a8d2df
         echo
-        
+
         echo '++++++++CCACHE++++++++'
         ccache -s || echo CCACHE NOT INSTALLED
-        
+
         echo '++++++++STORAGE++++++++'
         df -h
-        
+
         echo '++++++++H-TOP++++++++'
         top -b -i -H -n 1
-        
+
         echo '++++++++SYSTEM++++++++'
         cat /etc/os*
-        
+
         echo '++++++++ENV-stats++++++++'
         env
 } >>./stats.md
